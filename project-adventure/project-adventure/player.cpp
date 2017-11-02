e99f1a7bd63b61062c6909decbf596043e8ccaf8
Cory <33212192+pineauc@users.noreply.github.com>
2017-11-01 21:23:47 -0400

    Expanded upon the player class by adding more custom constructors, instance data and charcter body part functions.

--- project-adventure/project-adventure/player.cpp
+++ project-adventure/project-adventure/player.cpp
@@ -6,18 +6,44 @@
  *	Initializes a player object with a default name
  *		of "Traveler."
  */
-player::player() : 
-	name( "Traveler" ), hitpoints( 100 )
+player::player() :
+	name( "Traveler" ), health( 100 ),
+	armor( 100 ), multiplier( 0 )
 {
 }
 
 /**
- *	Initializes a player object with a player-defined
- *		character name.
+ *	Initializes a player object with a specified name.
  *	@param playerName -> the player's name
  */
 player::player( string playerName ) :
-	name( playerName ), hitpoints( 100 )
+	name( playerName ), health( 100 ),
+	armor( 100 ), multiplier( 0 )
+{
+}
+
+/**
+ *	Initializes a player object with a specified name
+ *		and health level.
+ *	@param	playerName	-> character name
+ *			hp			-> health level
+ */
+player::player( string playerName, int hp ) :
+	name( playerName ), health( hp ),
+	armor( 100 ), multiplier( 0 )
+{
+}
+
+/**
+ *	Initializes a player object with a specified name,
+ *		health level and armor level.
+ *	@param	playerName	-> character name
+ * 			hp			-> health level
+ *			ar			-> armor level
+ */
+player::player( string playerName, int hp, int ar ) :
+	name( playerName ), health( hp ),
+	armor( 100 ), multiplier( 0 )
 {
 }
 
@@ -29,20 +55,37 @@ player::~player()
 }
 
 /**
- *	Sets the player's hitpoint level.
- *	@param hp -> the new hitpoint level to set
+ *	Sets the player's health level.
+ *	@param hp -> the new health level to set
  */
-void player::setHitpoints( int hp )
+void player::setHealth( int hp )
 {
-	hitpoints = hp;
+	health = hp;
 }
 
 /**
- *	Returns a player's hitpoint level.
+ *	Sets the player's armor level.
+ *	@param ar -> the new armor level to set
  */
-int player::getHitpoints()
+void player::setArmor( int ar )
 {
-	return hitpoints;
+	armor = ar;
+}
+
+/**
+ *	Returns a player's health level.
+ */
+int player::getHealth()
+{
+	return health;
+}
+
+/**
+ *	Returns a player's armor level.
+ */
+int player::getArmor()
+{
+	return armor;
 }
 
 /**
--- project-adventure/project-adventure/player.h
+++ project-adventure/project-adventure/player.h
@@ -12,25 +12,43 @@ using std::string;
  */
 class player {
 public:
-	// Constructors
+	// Default constructor
 	player();
+
+	// Custom constructors
 	player( string );
+	player( string, int );
+	player( string, int, int );
 
 	// Destructor
 	~player();
 
 	/// Member functions
 	// Mutators
-	void setHitpoints( int );
+	void	setHealth( int );
+	void	setArmor( int );
 
 	// Accessors
-	int getHitpoints();
-	string getName();
+	int		getHealth();
+	int		getArmor();
+	string	getName();
+
+	// Character body
+	int		head();
+	int		torso();
+	int		r_hand();
+	int		l_hand();
+	int		r_leg();
+	int		l_leg();
+	int		r_foot();
+	int		l_foot();
 
 private:
 	// Instance data
-	string name;
-	int hitpoints;
+	string	name;		// Player's name
+	int		health;		// Player's health
+	int		armor;		// Player's armor
+	float	multiplier;	// Damage multiplier
 
 	// Mutators
 	void setName( string );
